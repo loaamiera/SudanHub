8e05dda4a04d2ec60182e81ec78e32bbb808087e
loaamiera <brownamiera@gmail.com>
2025-06-11 06:57:50 -0700

    Update and rename images.js to images1.js

---
+++ images1.js
@@ -0,0 +1,14 @@
+
+
+
+var alflag = document.getElementById("largeImage");
+
+var picture = document.getElementById("stages");
+
+altmessage.onclick = function flag() {
+  altmessage.innerHTML.src = 'diffFlag.png'
+
+} ;
+
+
+
