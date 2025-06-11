3d5df06793c0bcca3e0a8e8b9f3fe4ab49b0d2d2
loaamiera <brownamiera@gmail.com>
2025-06-11 08:25:47 -0700

    Update images1.js

--- images1.js
+++ images1.js
@@ -1,9 +1,8 @@
 
 
 
-var alflag = document.getElementById("largeImage");
+var flag = document.getElementById("largeImage");
 
-var picture = document.getElementById("stages");
 
 altmessage.onclick = function flag() {
   altmessage.innerHTML.src = 'diffFlag.png'
