02b5e6d3f1617079fe3a0f0634eabe14332933cd
loaamiera <brownamiera@gmail.com>
2025-06-14 19:34:49 -0700

    Update images1.js

--- images1.js
+++ images1.js
@@ -6,9 +6,9 @@ var flag = document.getElementById("largeImage");
 
 flag.onmouseover = function different() {
   flag.src = 'diffFlag.png' ;
-  flag.mouseleave = function back() {
+ } ;
+
+flag.onclick = function back() {
       flag.src = 'flag.png' ;
-} } ;
-
-
+}
 
